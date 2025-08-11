071869289a02ddcb6644e659cbeb0c7cd523fbb6
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-11 23:56:20 +0530

    Half Frontend Done

--- server/server.js
+++ server/server.js
@@ -8,20 +8,19 @@ import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-
 const port = process.env.PORT || 4000;
-
 connectDB();
+
+const allowedOrigins = ['http://localhost:5173']
 
 // Parse incoming JSON requests and put the parsed data in req.body
 app.use(express.json());
-
 // Parse cookies attached to the client request object (req.cookies)
 app.use(cookieParser());
-
 // Enable CORS and allow credentials (cookies, authorization headers, etc.) to be sent in cross-origin requests
 app.use(
   cors({ 
+    origin: allowedOrigins,
     // Allow requests from this origin
     credentials: true, // Allow cookies and auth headers in requests
   })
@@ -31,13 +30,10 @@ app.use(
 app.get("/", (req, res) => {
     res.send("API Working");
 });
-
 // Use the authRouter for all auth-related routes
 // All routes defined in authRouter will now be prefixed with /api/auth
 app.use("/api/auth", authRouter);
-
 app.use("/api/user", userRouter);
-
 
 app.listen(port, () => {
     console.log(`Server started on PORT : ${port}`);
